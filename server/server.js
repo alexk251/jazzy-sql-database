79a162c73fa9b3a4ed0f351e084ea5bc5b98e546
Alex Kim <alexkim251@yahoo.com>
2021-05-25 16:55:42 -0500

    got both get requestes to communicate with postico and append on the dom

--- server/server.js
+++ server/server.js
@@ -11,61 +11,120 @@ app.listen(PORT, () => {
     console.log('listening on port', PORT)
 });
 
+const pg = require('pg');
+//pg configuration LOCAL ONLY
+const Pool = pg.Pool;
+const pool = new Pool({
+    database: 'jazzy_sql', // THIS CHANGES BY PROJECT
+    host: 'localhost',
+    port: 5432,
+})
+
+pool.on('connect', () => {
+    console.log('CONNECTEDD TO POSTGRES');
+});
+
+pool.on('error', (error) => {
+    console.log(error);
+})
+
 // TODO - Replace static content with a database tables
-const artistList = [ 
-    {
-        name: 'Ella Fitzgerald',
-        birthdate: '04-25-1917'
-    },
-    {
-        name: 'Dave Brubeck',
-        birthdate: '12-06-1920'
-    },       
-    {
-        name: 'Miles Davis',
-        birthdate: '05-26-1926'
-    },
-    {
-        name: 'Esperanza Spalding',
-        birthdate: '10-18-1984'
-    },
-]
-const songList = [
-    {
-        title: 'Take Five',
-        length: '5:24',
-        released: '1959-09-29'
-    },
-    {
-        title: 'So What',
-        length: '9:22',
-        released: '1959-08-17'
-    },
-    {
-        title: 'Black Gold',
-        length: '5:17',
-        released: '2012-02-01'
-    }
-];
+// const artistList = [ 
+//     {
+//         name: 'Ella Fitzgerald',
+//         birthdate: '04-25-1917'
+//     },
+//     {
+//         name: 'Dave Brubeck',
+//         birthdate: '12-06-1920'
+//     },       
+//     {
+//         name: 'Miles Davis',
+//         birthdate: '05-26-1926'
+//     },
+//     {
+//         name: 'Esperanza Spalding',
+//         birthdate: '10-18-1984'
+//     },
+// ]
+// const songList = [
+//     {
+//         title: 'Take Five',
+//         length: '5:24',
+//         released: '1959-09-29'
+//     },
+//     {
+//         title: 'So What',
+//         length: '9:22',
+//         released: '1959-08-17'
+//     },
+//     {
+//         title: 'Black Gold',
+//         length: '5:17',
+//         released: '2012-02-01'
+//     }
+// ];
 
 app.get('/artist', (req, res) => {
     console.log(`In /songs GET`);
-    res.send(artistList);
+    const queryText = `SELECT * FROM "artists" ORDER BY "id" DESC;`
+    pool.query(queryText)
+    .then( (result) => {
+        //only guarentee that the query is done
+        console.log(result.rows); // result.rows is our selected data
+        res.send(result.rows);
+    }).catch((err) =>{
+        console.log(err);
+        res.sendStatus(500);
+    })
 });
 
 app.post('/artist', (req, res) => {
-    artistList.push(req.body);
-    res.sendStatus(201);
+    let queryText = `INSERT INTO "songs" ("name", "brithdate")
+    VALUES ($1, $2);`
+
+    let values = [req.body.name, req.body.birthdate];
+    
+    pool.query(queryText, values)
+    .then( (result) => {
+        //only guarentee that the query is done
+        //console.log(result.rows); // result.rows is our selected data
+        res.sendStatus(201);
+    }).catch((err) =>{
+        console.log(err);
+        res.sendStatus(500);
+    })
 });
 
 app.get('/song', (req, res) => {
     console.log(`In /songs GET`);
-    res.send(songList);
+    const queryText = `SELECT * FROM "songs" ORDER BY "id" DESC;`
+    pool.query(queryText)
+    .then( (result) => {
+        //only guarentee that the query is done
+        console.log(result.rows); // result.rows is our selected data
+        res.send(result.rows);
+    }).catch((err) =>{
+        console.log(err);
+        res.sendStatus(500);
+    })
 });
 
 app.post('/song', (req, res) => {
-    songList.push(req.body);
-    res.sendStatus(201);
+    let queryText = `INSERT INTO "songs" ("rank", "track", "artist", "published")
+    VALUES ($1, $2, $3, $4);`
+
+    let values = [req.body.rank, req.body.track, req.body.artist, req.body.published]
+
+    pool.query(queryText, values)
+    .then( (result) => {
+        //only guarentee that the query is done
+        //console.log(result.rows); // result.rows is our selected data
+        res.sendStatus(201);
+    }).catch((err) =>{
+        console.log(err);
+        res.sendStatus(500);
+    })
 });
 
 
